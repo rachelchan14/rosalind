3d148ea23e0766c6589988d1736e5a6e50879dd9
rachelchan14 <44048826+rachelchan14@users.noreply.github.com>
2022-12-19 23:52:39 -0800

    Update and rename main.py to Counting DNA Nucleotides/dna.py

---
+++ Counting DNA Nucleotides/dna.py
@@ -0,0 +1,19 @@
+from tkinter import filedialog
+from test import x
+
+def openfilename():
+    path = filedialog.askopenfilename()
+    return path
+
+def countingDNAnucleotides(filename):
+    nucleotides = open(filename).read()
+    # my_dict = {}
+    # for i in nucleotides:
+    #     if (i == '\n'):
+    #         continue
+    #     my_dict[i] = my_dict.get(i, 0) + 1
+    # ordered = sorted(my_dict.items())
+    # dict(ordered)
+    # for value in ordered:
+    #     print (value[1], end = " ")
+    print(nucleotides.count('A'), nucleotides.count('C'), nucleotides.count('G'), nucleotides.count('T')) #kind of like get but in non-dictionary version
