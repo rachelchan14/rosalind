02702cf7959e7b6204b1e9ec68d4bd451b52690e
rachelchan14 <44048826+rachelchan14@users.noreply.github.com>
2022-12-20 00:18:09 -0800

    format dna question

--- Counting DNA Nucleotides/dna.py
+++ Counting DNA Nucleotides/dna.py
@@ -1,12 +1,12 @@
 from tkinter import filedialog
-from test import x
 
-def openfilename():
+def openfilename(): #toopen file via file dialog rather than path name
     path = filedialog.askopenfilename()
     return path
 
 def countingDNAnucleotides(filename):
     nucleotides = open(filename).read()
+    output = open("dna_output.txt", "w")
     # my solution: 
     # my_dict = {}
     # for i in nucleotides:
@@ -19,4 +19,12 @@ def countingDNAnucleotides(filename):
     #     print (value[1], end = " ")
     
     # cleaner solution:
-    print(nucleotides.count('A'), nucleotides.count('C'), nucleotides.count('G'), nucleotides.count('T')) #kind of like get but in non-dictionary version
+    bases = ['A', 'C', 'G', 'T']
+    list = []
+    for i in bases:
+        list.append(str(nucleotides.count(i)))
+    output.write(" ".join(list))
+        #count = kind of like get but in non-dictionary version
+
+file = openfilename()
+countingDNAnucleotides(file)
